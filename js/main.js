93bf9430c234a9e6d90837d5357049a9fdd661b8
ebhinch <erica.hinchman@gmail.com>
2017-06-25 12:54:19 -0400

    created game logic. completed unit 11 6.25.16

--- js/main.js
+++ js/main.js
@@ -24,7 +24,7 @@ var cards = [
 var cardsInPlay = [];
 
 var checkForMatch = function() {
-	if (cardsInPlay[0] === cardsInPlay[1]) { 
+	if (cardsInPlay[0]=== cardsInPlay[1]) { 
 		alert("You've found a match!");
 		} else{
 			alert("Sorry, try again.");
@@ -38,10 +38,33 @@ var flipCard = function(cardId) {
 	cardsInPlay.push(cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
+	cardsInPlay.push(cards[cardId].suit);
 
 
 	checkForMatch();
 };
+
+// In the main.js file in Sublime Text, add a function that will create the game board. Right after the flipCard function, add a new function createBoard. This function will not have any parameters.
+var createBoard = function()
+{
+	
+	//Now add each card to the board.
+	//Within the createBoard function, use a for loop to loop through the cards array.
+	for (var i=0; i<cards.length; i++)
+
+	//Now use the createElement method to create an img element and store it in a variable cardElement.
+	var cardElement = document.createElement(img);
+
+	//Now use the setAttribute() method to add a src attribute for the cardElement. The src should be "images/back.png"
+	cardElement.setAttribute('cardElement', 'images/back.png');
+
+	//Now use the setAttribute() method once again to set the card's 'data-id' attribute to be the index of the current element, i (no quotes).
+	//Note: data- attributes are meant to store data about an element that is not tied to a style.
+	cardElement.setAttribute(data-id, cards[i]);
+
+
+}
+
 	flipCard(0);
 	flipCard(2);
 
